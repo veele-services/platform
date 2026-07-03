57e04ae775e4617da4d949fbd56025bc5c2dc936
TIXOCEO <famgoldenbelt@gmail.com>
2026-07-03 08:50:07 +0200

    db: accept legacy tenant uuid forms in storage paths

--- lib/db/src/storage-paths.ts
+++ lib/db/src/storage-paths.ts
@@ -1,4 +1,4 @@
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu;
+const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/iu;
 const URL_SCHEME_PATTERN = /^[a-z][a-z\d+.-]*:\/\//iu;
 
 export const FIELDGRID_STORAGE_TENANT_ROOT = "tenant";
